0b4a645077631b7661d4daf5747007c4f7181bb1
Bhavna <bhavna.chaudhary@xebia.com>
2023-04-24 21:06:42 +0530

    updated styles & values unit

--- src/app/components/TodaysHighlight.js
+++ src/app/components/TodaysHighlight.js
@@ -8,26 +8,41 @@ export function TodaysHighlight() {
 
     const [sunriseTime, setSunriseTime] = useState(null);
     const [sunsetTime, setSunsetTime] = useState(null);
+    const [visibility, setVisibility] = useState(null);
+    const [windSpeed, setWindSpeed] = useState(null);
 
     useEffect(() => {
+        // ------- set sunrise time ---------
         const sunrise = new Date(weatherData?.sys?.sunrise * 1000);
         setSunriseTime(moment(sunrise).format('h:mm A'));
+
+        // ------- set sunset time ---------
         const sunset = new Date(weatherData?.sys?.sunset * 1000);
         setSunsetTime(moment(sunset).format('h:mm A'));
+
+        // ------- set visibility in km ----------
+        let vis = weatherData?.visibility;
+        vis = vis / 1000;
+        setVisibility(vis + ' km');
+
+        // ------- set wind speed in km/h ----------
+        let ws = weatherData?.wind?.speed;
+        ws = (ws * 3600) / 1000;
+        setWindSpeed(ws)
     }, [weatherData]);
 
     const bottomData = [
         {
             title: "Humidity",
-            value: weatherData?.main?.humidity
+            value: weatherData?.main?.humidity + '%',
         },
         {
             title: "Visibility",
-            value: weatherData?.visibility
+            value: visibility,
         },
         {
             title: "Feels like",
-            value: weatherData?.main?.feels_like
+            value: weatherData?.main?.feels_like + '°C',
         },
     ];
 
@@ -35,8 +50,8 @@ export function TodaysHighlight() {
         const { title, value } = props;
         return (
             <div className='BottomContainersDiv'>
-                <p>{title}</p>
-                <p>{value}</p>
+                <p className='BottomContainersDivTitle'>{title}</p>
+                <p className='BottomContainersDivValue'>{value}</p>
             </div>
         );
     };
@@ -48,18 +63,18 @@ export function TodaysHighlight() {
             <div className='todaysHighlightBody'>
                 <div className='windStatus'>
                     <p className='windStatusTitle'> Wind Status</p>
-                    <p className='windStatusValue'> {weatherData?.wind?.speed}</p>
+                    <p className='windStatusValue'> {`${windSpeed} km/h`}</p>
                 </div>
                 <div className='sunriseSunset'>
                     <p className='sunriseSunsetTitle'>Sunrise & Sunset</p>
                     <div className='sunriseSunsetBottomDiv'>
                         <div className='sunriseDiv'>
-                            <p>Sunrise</p>
-                            <p>{sunriseTime}</p>
+                            <p className='sunriseTitle'>Sunrise</p>
+                            <p  className='sunriseValue'>{sunriseTime}</p>
                         </div>
                         <div className='sunsetDiv'>
-                            <p> Sunset</p>
-                            <p>{sunsetTime}</p>
+                            <p  className='sunsetTitle'> Sunset</p>
+                            <p  className='sunsetValue' >{sunsetTime}</p>
                         </div>
                     </div>
                 </div>
